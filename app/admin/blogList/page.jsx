89ff664400013ab055c7c0bc843ae65b3869ce04
hashirkamilbaig <hashirkamilbaig2003@gmail.com>
2024-12-01 19:49:43 -0800

    completed blog list page where it using api and database to map through all the blogs and show them on the page. Date to string was also fixed

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,7 +1,20 @@
 'use client'
-import React from 'react'
+import BlogTableItem from '@/components/AdminComponents/BlogTableItem'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const page = () => {
+  const [blogs,setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    const response = await axios.get('/api/blog');
+    setBlogs(response.data.blogs)
+  }
+
+  useEffect(() => {
+    fetchBlogs()
+  },[]);
+
   return (
     <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
       <h1>All Blogs</h1>
@@ -23,6 +36,11 @@ const page = () => {
               </th>
             </tr>
           </thead>
+          <tbody>
+            {blogs.map((item, index)=>{
+              return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date}/>
+            })}
+          </tbody>
         </table>
       </div>
     </div>
